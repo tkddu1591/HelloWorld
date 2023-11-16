34ce44a0a80a966c24704142b6b05547b57b5896
김상엽_학원 <tkddu1591@gmail.com>
2023-11-16 18:07:01 +0900

    Lecture DB 작성

--- front/src/pages/lecture/write/LectureWrite.tsx
+++ front/src/pages/lecture/write/LectureWrite.tsx
@@ -9,8 +9,8 @@ function LectureWrite() {
 
     const [tagSort, setTagSort] = useState(
         {
-            placeholder: '태그 선택',
-            list: [
+            placeholder:  '태그 선택',
+            list:         [
                 {value: 'all', label: '전체'},
                 {value: 'apl', label: '지원완료'},
                 {value: 'res', label: '서류통과'},
@@ -20,33 +20,49 @@ function LectureWrite() {
                 {value: 'chk', label: '검토중'},
             ],
             isSearchable: false,
-            isMulti: true,
+            isMulti:      true,
         }
     );
     const [levelSort, SetlevelSort] = useState(
         {
-            placeholder: '레벨 선택',
-            list: [
+            placeholder:  '레벨 선택',
+            list:         [
                 {value: '0', label: '입문'},
                 {value: '1', label: '초급'},
                 {value: '2', label: '중급'},
                 {value: '3', label: '고급'},
             ],
             isSearchable: false,
-            isMulti: true,
+            isMulti:      true,
         }
     );
     return <Container style={{marginTop: '100px'}}>
         <Row>
             <Col className={'lectureWrite'}>
                 <h3>강의 등록</h3>
+            </Col>
+
+        </Row>
+        <Row style={{marginTop:'20px'}}>
+            <Col md={4}>
+
                 <div className={'dataChange'}>
-                <SelectBox options={tagSort.list} isMulti={tagSort.isMulti}
-                           placeholder={tagSort.placeholder} isSearchable={tagSort.isSearchable}></SelectBox>
+                    <SelectBox options={tagSort.list} isMulti={tagSort.isMulti}
+                               placeholder={tagSort.placeholder} isSearchable={tagSort.isSearchable}></SelectBox>
                 </div>
+            </Col>
+            <Col md={4}>
+
                 <div className={'dataChange'}>
-                <SelectBox options={levelSort.list}
-                           placeholder={levelSort.placeholder} isSearchable={levelSort.isSearchable}></SelectBox>
+                    <SelectBox options={levelSort.list}
+                               placeholder={levelSort.placeholder} isSearchable={levelSort.isSearchable}></SelectBox>
+                </div>
+            </Col>
+            <Col md={4}>
+
+                <div className={'dataChange'}>
+                    <SelectBox options={levelSort.list}
+                               placeholder={levelSort.placeholder} isSearchable={levelSort.isSearchable}></SelectBox>
                 </div>
             </Col>
         </Row>
