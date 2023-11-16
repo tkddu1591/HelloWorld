e29f5351ca6f9788ced1475316483e5cf39b136e
haminsu <ainu90@gmail.com>
2023-11-16 10:28:11 +0900

    페이지 디자인

--- front/src/pages/codingtest/table/CodeListTable.js
+++ front/src/pages/codingtest/table/CodeListTable.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import Axios from "axios";
 import Pagination from "react-js-pagination";
+import "../Pagination/PageStyle.css"
 
 
 function CodeListTable() {
