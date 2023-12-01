8cd6ec9edefba20efbd0eb95f2d459e8e65eee16
김상엽_학원 <tkddu1591@gmail.com>
2023-12-01 12:24:32 +0900

    websocket 구현

--- front/src/pages/dm/Chat/DmChat.tsx
+++ front/src/pages/dm/Chat/DmChat.tsx
@@ -35,7 +35,7 @@ function DmChat() {
 
     const webHost = window.location.hostname === "localhost"
         ? '//localhost:8080'
-        : "//52.78.236.129:8080";
+        : "/api";
     // @ts-ignore
     const webSocketLogin = useCallback(() => {
         ws.current = new WebSocket(`ws:${webHost}/socket/chatt`);
