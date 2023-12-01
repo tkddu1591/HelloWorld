adba2d665689eaa3140053d7430448deda117424
lomong7807 <lomong7807@gmail.com>
2023-12-01 09:56:12 +0900

    update

--- front/src/store.js
+++ front/src/store.js
@@ -2,7 +2,6 @@ import {configureStore} from "@reduxjs/toolkit";
 import {myIp} from "./slice/myIpSlice";
 import {myInfo} from "./slice/UserSlice";
 import {myCartTotalSlice, myCartSlice} from "./slice/myCartSlice";
-import {partListSlice, contentListSlice} from "./slice/LectureContent"
 import {cateNo, changeCateNo} from "./slice/CommunityCateNo"
 import {listURL, changeURL} from "./slice/CommunityListURL";
 import {partListSlice, contentListSlice, lectureIHeardList, contentCount} from "./slice/LectureContent"
